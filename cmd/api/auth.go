ea080a4b0fa1e585d71c05fb8c3ab07fe60e7e67
TedrosHG <tedrosastu@gmail.com>
2026-05-11 12:23:44 +0300

    <fix> update the type for register user payload

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -10,9 +10,9 @@ import (
 )
 
 type RegisterUserPayload struct {
-	Username string `json:"username" validate="required,max=100"`
-	Email    string `json:"email" validate="required,email,max=255"`
-	Password string `json:"password" validate="required,min=3,max=72"`
+	Username string `json:"username" validate:"required,max=100"`
+	Email    string `json:"email" validate:"required,email,max=255"`
+	Password string `json:"password" validate:"required,min=3,max=72"`
 }
 
 type UserWithToken struct {
